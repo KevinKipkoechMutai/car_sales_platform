f99f1267cfc071f0a289fad2ad2f08676a0cb77c
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-09-16 23:26:34 +0300

    search function

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,9 +1,17 @@
 "use client"
+import Image from "next/image"
 import { SearchManufacturer } from "."
 import React, {useState} from "react"
 
+const SearchButton = ({otherClasses}: {otherClasses: string}) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image src="/magnifying-glass.svg" alt="magnifying glass" width={40} height={40} className="object-contain"/>
+  </button>
+)
+
 const Searchbar = () => {
     const [manufacturer, setManufacturer] = useState("")
+    const [model, setModel] = useState("")
     const handleSearch = () => {}
   return (
     <form className='searchbar' onSubmit={handleSearch}>
@@ -12,7 +20,16 @@ const Searchbar = () => {
                 manufacturer={manufacturer}
                 setManufacturer={setManufacturer}
             />
+            <SearchButton otherClasses="sm:hidden"/>
         </div>
+        <div className="searchbar__item">
+          <Image src="/model-icon.png" alt="model" width={25} height={25} className="absolute w-[20px] h-[20px] ml-4"/>
+          <input type="text" name="model" value={model} onChange={(e) => setModel(e.target.value)} placeholder="Tiguan" 
+            className="searchbar__input"
+          />
+          <SearchButton otherClasses="sm:hidden"/>
+        </div>
+        <SearchButton otherClasses="max-sm:hidden"/>
     </form>
   )
 }
